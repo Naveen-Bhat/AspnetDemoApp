53f6cb0dcc31695e405cf90a7df0eed0fb54d0ab
Naveen Bhat <knaveenbhat@gmail.com>
2018-08-03 16:58:04 +0530

    Initial Setup

---
+++ DemoApp/Controllers/FooController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApp.Controllers
+{
+    public class FooController : Controller
+    {
+        public IActionResult Bar()
+        {
+            return View();
+        }
+    }
+}
