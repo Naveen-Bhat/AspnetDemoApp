06c7016b09ad5e6965fa486b25a320306536b982
Naveen Bhat <knaveenbhat@gmail.com>
2018-08-08 13:01:17 +0530

    Ajax get and post.

--- DemoApp/Controllers/PersonController.cs
+++ DemoApp/Controllers/PersonController.cs
@@ -10,6 +10,8 @@ namespace DemoApp.Controllers
 {
     public class PersonController : Controller
     {
+        private static List<PersonViewModel> people = new List<PersonViewModel>();
+
         public IActionResult Index()
         {
             return View();
@@ -36,7 +38,21 @@ namespace DemoApp.Controllers
         [HttpPost]
         public JsonResult SavePerson(PersonViewModel viewModel)
         {
+            people.Add(viewModel);
             return Json(new { data = "Saved successfully!" });
+        }
+
+        [HttpGet]
+        public JsonResult GetPeople()
+        {
+            return Json(people);
+        }
+
+        public JsonResult Search(string pattern)
+        {
+            var result = people.Where(x => x.Name.Contains(pattern));
+
+            return Json(result);
         }
     }
 }
