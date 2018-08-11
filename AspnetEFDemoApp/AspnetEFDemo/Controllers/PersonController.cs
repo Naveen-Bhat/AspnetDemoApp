33e450273cf511f1f5e15c02ac48830cf156a532
Naveen Bhat <knaveenbhat@gmail.com>
2018-08-11 13:23:47 +0530

    Implemented CRUD Operations.

--- AspnetEFDemoApp/AspnetEFDemo/Controllers/PersonController.cs
+++ AspnetEFDemoApp/AspnetEFDemo/Controllers/PersonController.cs
@@ -12,10 +12,85 @@ namespace AspnetEFDemo.Controllers
         // GET: Person
         public ActionResult Index()
         {
-            var entitis = new InternshipDbEntities();
-            var people = entitis.People.ToList();
+            List<Person> people = null;
+
+            using (var context = new InternshipDbEntities())
+            {
+                people = context.People.ToList();
+            }
 
             return View(people);
+        }
+
+        public ActionResult Create(Person person)
+        {
+            return View(person);
+        }
+
+        public ActionResult Update(int personId)
+        {
+            Person person = null;
+
+            using (var context = new InternshipDbEntities())
+            {
+                person = context.People.Where(x => x.Id == personId).SingleOrDefault();
+            }
+
+            return View(person);
+        }
+
+        [HttpPost]
+        public ActionResult SavePerson(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var context = new InternshipDbEntities())
+                {
+                    context.People.Add(person);
+                    context.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            return View("CreateOrUpdate", person);
+        }
+
+        [HttpPost]
+        public ActionResult UpdatePerson(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var context = new InternshipDbEntities())
+                {
+                    var personObj = context.People.Where(x => x.Id == person.Id).SingleOrDefault();
+
+                    if (personObj != null)
+                    {
+                        personObj.Name = person.Name;
+                        context.SaveChanges();
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            return View("CreateOrUpdate", person);
+        }
+
+        public JsonResult Delete(int personId)
+        {
+            using (var context = new InternshipDbEntities())
+            {
+                var person = context.People.Where(x => x.Id == personId).SingleOrDefault();
+                if (person != null)
+                {
+                    context.People.Remove(person);
+                    context.SaveChanges();
+                }
+            }
+
+            return Json(new { }, JsonRequestBehavior.AllowGet);
         }
     }
 }
