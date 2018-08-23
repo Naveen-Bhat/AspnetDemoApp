56bc2d2f019e1495a3db39acb121b5d249b2fa48
Naveen Bhat <knaveenbhat@gmail.com>
2018-08-23 13:34:27 +0530

    Generics and Delegates

---
+++ CSharpDemoApp/CSharpDemoApp/Oops.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDemoApp
+{
+    public class Runner
+    {
+        public void Run()
+        {
+            Vehicle v = new Car("Alto");
+
+            MoveVechicle(v);
+
+            v = new Bike("Discover");
+
+            MoveVechicle(v);
+        }
+
+        static void MoveVechicle(Vehicle vehicle)
+        {
+            vehicle.Move();
+        }
+    }
+
+    public interface IStoppable
+    {
+        void Stop();
+    }
+
+    public interface ICkickStart
+    {
+        void CickStart();
+    }
+
+
+    public abstract class Vehicle : IStoppable
+    {
+        protected string name;
+
+        public Vehicle(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public virtual void Move()
+        {
+            Console.WriteLine(this.name + " is moving");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stopped");
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public Car(string name) : base(name)
+        {
+
+        }
+
+        public override void Move()
+        {
+            throw new Exception("Accident");
+        }
+
+        public void Foo()
+        {
+
+        }
+    }
+
+    public class Bike : Vehicle, ICkickStart
+    {
+        public Bike(string name) : base(name)
+        {
+
+        }
+
+        public void CickStart()
+        {
+            Console.WriteLine("Cick start");
+        }
+    }
+}
---
+++ CSharpDemoApp/CSharpDemoApp/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDemoApp
+{
+    public class MyList<T>
+    {
+        private T[] data;
+        private int iterator = 0;
+
+        public MyList()
+        {
+            this.data = new T[10];
+        }
+
+        public MyList(int size)
+        {
+            this.data = new T[size];
+        }
+
+        public void Add(T d)
+        {
+            this.data[iterator] = d;
+            iterator++;
+        }
+
+        public T[] Where(Func<T, bool> filter)
+        {
+            T[] output = new T[this.data.Length];
+            int oIterator = 0;
+
+            for (var i = 0; i < this.data.Length; i++)
+            {
+                if (filter(this.data[i]))
+                {
+                    output[oIterator] = this.data[i];
+                    oIterator++;
+                }
+            }
+
+            return output;
+        }
+
+        public T[] GetAll()
+        {
+            T[] output = new T[this.data.Length];
+            for (var i = 0; i < this.data.Length; i++)
+            {
+                output[i] = this.data[i];
+            }
+
+            return output;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new MyList<int>();
+
+            list.Add(1);
+            list.Add(11);
+            list.Add(10);
+            list.Add(91);
+
+            Console.WriteLine("All");
+            var data = list.GetAll();
+            Print(data);
+
+            Console.WriteLine("Even");
+            var evenNumbers = list.Where(x => x % 2 == 0);
+            Print(evenNumbers);
+
+            Console.WriteLine("Odd");
+            var odd = list.Where(x => x % 2 != 0);
+            Print(odd);
+
+            Console.ReadKey();
+        }
+
+        static void Print<T>(T[] data)
+        {
+            foreach (var d in data)
+            {
+                if (d.ToString() != "0")
+                {
+                    Console.WriteLine(d);
+                }
+            }
+        }
+    }
+}
